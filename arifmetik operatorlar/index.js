e9498caedb46a284c14212bf780f6809a013f723
asror-developer <asroreshnazarov8@gmail.com>
2024-05-08 09:46:34 +0500

    arfmetik operatorlar

---
+++ arifmetik operatorlar/index.js
@@ -0,0 +1,224 @@
+//  1 masala
+//  let R=5;
+//  let L=2*Math.PI*R;
+//  let s=Math.PI*R**2
+//  console.log(L,s);
+
+// 2 masala
+// let a=25;
+// let b=4;
+// let G=Math.sqrt(a*b);
+// console.log(G);
+
+// 3 masala
+// let x=3;
+// let y=3*x**6-6*x**2-7;
+// console.log(y);
+
+// 4 masala
+// let x=4;
+// let a=1000;
+// let result=a/x;
+// console.log(result);111111111111
+
+// 5 masala
+// let a=7;
+// let b=5;
+// let c=2;
+// a=a-c;
+// b=a+c;
+// console.log(a,b);
+ 
+// integer1 masala
+// let a = 110
+// let b = Math.floor(a/10);
+// console.log(b); 
+
+// let c = 130;
+// let h = c%10;
+// console.log(h);
+    
+// let s = b+h 
+// console.log(s);
+
+// integer5
+
+
+
+
+
+// ---------------------------------------uyga vazifa-------------
+//  1masala
+// let a=5;
+// let b=7;
+// let s=a*b;
+// let p=2*(a+b);
+// console.log(s,p);
+
+
+//  2masala
+// let d=6;
+// let r=3;
+// let l=3.14*d;
+// let s=3.14*r**2;
+// console.log(l,s);
+
+// 3masala
+// let a=8;
+// let V=a**2;
+// let S=6*a**2;
+// console.log(V,S);
+
+// 4masala
+// let a=4;
+// let b=5;
+// let c=6;
+// let V=a*b*c;
+// let S=2*(a*b+b*c+a*c);
+// console.log(V,S);
+
+// 5masala
+// let a=8;
+// let b=22;
+// let M=(a+b)/2;
+// console.log(M);
+
+// 6masala
+//  let a=8;
+//  let b=6;
+//  let c=12;
+//  let M=Math.sqrt(a**2+b**2);
+//  let P=a+b+c;
+//  console.log(M,P);
+
+
+// 7masala
+// let r1=9;
+// let r2=8;
+// let S1=3.14*r1;
+// let S2=3.14*r2;
+// let S=3.14*(r1**2-r2**2);
+// console.log(S);
+
+
+// 8masala
+// let L=10;
+// let R=L/(2*3.14);
+// let S=3.14*R**2;
+// console.log(R,S);
+
+
+// 9masala
+// let x1=8;
+// let x2=6;
+// let y1=10;
+// let y2=6;
+// let L=Math.sqrt(x1-x2)**2+(y1-y2)**2
+// console.log(L);
+
+
+// 10masala
+// let A=20;
+// let B=7;
+// let C=13;
+//  A=A-C;
+//  B=A-B;
+//  C=B+C;
+//  console.log(A,B,C);11111111111111111111111111
+
+
+// 11masala
+// let x=4;
+// let y=4*(x-3)**6-7*(x-3)**3+2;
+// console.log(y);
+
+
+// 12masala
+// let A=2;
+// let X=A**3;
+// let Y=A**6;
+// let Z=A**9;
+// let M=A**15;
+// console.log(X,Y,Z,M);
+
+
+// 13masala
+// let Tf=41;
+// let Tc=(Tf-32)*5/9;
+// console.log(Tc);
+
+// 14masala
+// let X=10;
+// let Y=8;
+// let U=(X-Y);
+// console.log(U);
+
+
+// 15masala
+// let X=29;
+// let Y=4;
+// let J=(X+Y);
+// console.log(J);
+
+
+// ------------------------------------
+
+
+// intejer1
+// let a=225;
+// let b=225;
+// let x=Math.floor(a%10);
+// let y=Math.floor(a%100);
+// console.log(x,y); 
+
+// integer2
+// a=Math.floor(156);
+// b= Math.floor(a%10);
+// o=Math.floor((a%100)/10)
+// y=Math.floor((a%1000)/100)
+// console.log(b+o+y);
+
+
+// intejer3
+//  a=Math.floor(100);
+//  b= Math.floor(a % 10);
+//  console.log(b);
+//  o=Math.floor((a%100)/10)
+//  console.log(o);
+//  y=Math.floor((a%1000)/100)
+//  console.log(y);
+//  console.log(b,o,y);
+
+
+// // intejer4
+// a=Math.floor(173);
+// b= Math.floor(a/10);
+// o=Math.floor(a/100)
+// y=Math.floor(a/1000)
+// console.log(o,y,b);
+
+// integer7
+// let N=180;
+// let m=Math.floor(N/60)
+// console.log(m);
+
+
+// integer8
+//  let N=7200;
+//  let s=Math.floor(N/3600);
+//  console.log(s);
+ 
+
+// integer9
+// let N=1800;
+// let m=Math.floor(N/180);
+// let s=Math.floor(N/60);
+// console.log(m,s);
+
+
+// integer10
+// let N=3600;
+// let m=Math.floor(N/180);
+// let s=Math.floor(N/60);
+// console.log(m,s);
+
